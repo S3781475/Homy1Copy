2af8695d38e8f3c83edd65f29f295e27c9495d7e
S3781475 <s3781475@student.rmit.edu.au>
2022-09-21 02:10:21 +1000

    Updates

--- BackEnd/src/test/java/com/example/homy1/Homy1ApplicationTests.java
+++ BackEnd/src/test/java/com/example/homy1/Homy1ApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.homy1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Homy1ApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1,1);
     }
 
 }
